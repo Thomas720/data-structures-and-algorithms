1825545b74c72803675b6a913b605df5feb2ee26
Thomas Tilahun <thomm.sef@gmail.com>
2020-03-20 22:58:55 -0700

    array-shift passed code

---
+++ challenges/arrayShift/__tests__/array-shift.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const arrayShift = require('../array-shift');
+
+describe('Test Array-Shift', () => {
+    it('It should add an element in the middle of the current array', () => {
+        expect([1,2,3,4,9,5,6,7,8]).toBeTruthy();
+    })
+})
