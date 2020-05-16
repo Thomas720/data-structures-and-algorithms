185de627927c7be9142afb4607e9884cd3d242f0
Thomas Tilahun <thomm.sef@gmail.com>
2020-05-15 20:45:46 -0700

    done

--- data-structures/Breadth-first/breadth-first.js
+++ data-structures/Breadth-first/breadth-first.js
@@ -1,37 +1,76 @@
-class Tree {
-    constructor(value, left, right) {
-      this.value = value;
-      this.left = left;
-      this.right = right;
-    }
-  }
-  
-  const breadthFirstTraversal = (tree, callback) => {
-    if (tree == null) {
-      return;
-    }
-  
-    let queue = [tree]
-  
-    while (queue.length > 0) {
-      let item = queue.shift()
-      let value = item.value
-      callback(value)
-  
-      if (item.left == null && item.right == null) {
-        continue
-      }
-      if (item.left != null) {
-        queue.push(item.left)
-      }
-      if (item.right != null) {
-        queue.push(item.right)
-      }
-    }
-  }
+'use strict';
 
-  t = new Tree(1,
-    new Tree(2, null, null), new Tree(3,
-      new Tree(4, null, null), null))
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-breadthFirstTraversal(t, console.log)
+class QNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+    }
+
+    enqueue(val) {
+       let newNode = null;
+        if (!this.front && !this.rear) {
+            this.front = newNode;
+            this.rear = newNode;
+            return;
+        }
+
+        this.rear.next = newNode;
+        this.rear = newNode;
+    }
+
+    dequeue() {
+       
+        if (!this.front && !this.rear) return;
+
+        let oldFront = this.front;
+        this.front = this.front.next;
+
+        oldFront.next = null;
+        return oldFront.val;
+    
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    breadthFirst(root = this.root) {
+        let printedArr = [];
+        if (!root) return;
+
+        let q = new Queue();
+        q.enqueue(root); 
+
+
+        while (q.front) {
+           
+            if (q.front.val.left) q.enqueue(q.front.val.left);
+           
+            if (q.front.val.right) q.enqueue(q.front.val.right);
+
+            let removedItem = q.dequeue();
+            printedArr.push(removedItem.val);
+        }
+
+        return printedArr;
+    }
+}
+
+module.exports = { TreeNode, QNode, BinaryTree };
